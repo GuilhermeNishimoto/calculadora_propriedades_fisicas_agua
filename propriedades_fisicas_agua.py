f9f583980cf84ae98b6ecbeb7c562414f665037f
GuilhermeNishimoto <nishimotoguilherme@gmail.com>
2022-06-20 21:21:24 -0300

    class PropriedadesFisicasAgua

--- propriedades_fisicas_agua.py
+++ propriedades_fisicas_agua.py
@@ -4,25 +4,30 @@
 from math import e
 import pandas as pd
 
-def densidade(temperatura):
-    # Retorna o valor da densidade da água em kg/m³
-    return -0.000000125767*(temperatura**4)+0.000177892106*(temperatura**3)-0.096908373061*(temperatura**2)+23.424785483973*(temperatura)-1093.53530316845
+class PropriedadesFisicasAgua():
+    
+    def __init__(self, temperatura):
+        self.temperatura = temperatura
 
-def calor_especifico(temperatura):
-    # Retorna o valor do calor específico da água em kJ/kg K
-    return 0.000000002784026*(temperatura**4)-0.000003736020793*(temperatura**3)+0.001884889509818*(temperatura**2)-0.42325554048777*(temperatura)+39.8390948742563
+    def densidade(self):
+        # Retorna o valor da densidade da água em kg/m³
+        return -0.000000125767*(self.temperatura**4)+0.000177892106*(self.temperatura**3)-0.096908373061*(self.temperatura**2)+23.424785483973*(self.temperatura)-1093.53530316845
 
-def viscosidade(temperatura):
-    # Retorna a viscosidade da água em u Pa s 
-    return 0.0000316379321*(temperatura**4)-0.0433590650205*(temperatura**3)+22.325809094914*(temperatura**2)-5124.88587735*(temperatura)+443419.505781729
+    def calor_especifico(self):
+        # Retorna o valor do calor específico da água em kJ/kg K
+        return 0.000000002784026*(self.temperatura**4)-0.000003736020793*(self.temperatura**3)+0.001884889509818*(self.temperatura**2)-0.42325554048777*(self.temperatura)+39.8390948742563
 
-def condutividade_termica(temperatura):
-    # Retorna a condutividade térmica da água em J/s m K 
-    return -0.0000000046*(temperatura**3)-0.0000051588*(temperatura**2)+0.0059627933*(temperatura)-0.5906098532
+    def viscosidade(self):
+        # Retorna a viscosidade da água em u Pa s 
+        return 0.0000316379321*(self.temperatura**4)-0.0433590650205*(self.temperatura**3)+22.325809094914*(self.temperatura**2)-5124.88587735*(self.temperatura)+443419.505781729
 
-def pressao_vapor(temperatura):
-    # Retorna a pressão de vapor da água em kPa
-    return (101.325/760)*(e**(18.3036-(3816.44/(temperatura-46.13))))
+    def condutividade_termica(self):
+        # Retorna a condutividade térmica da água em J/s m K 
+        return -0.0000000046*(self.temperatura**3)-0.0000051588*(self.temperatura**2)+0.0059627933*(self.temperatura)-0.5906098532
+
+    def pressao_vapor(self):
+        # Retorna a pressão de vapor da água em kPa
+        return (101.325/760)*(e**(18.3036-(3816.44/(self.temperatura-46.13))))
 
 if (__name__ == '__main__'):
 
@@ -38,6 +43,8 @@ if (__name__ == '__main__'):
             print('\nA temperatura deve ser menor ou igual a 100°C\n')
         else:
             temperatura +=  273.15
+            
+            calculator = PropriedadesFisicasAgua(temperatura)
 
             while True:
                         
@@ -51,22 +58,22 @@ if (__name__ == '__main__'):
                 escolha = int(input('\nPropriedade Física: '))
 
                 if escolha == 1:
-                    print('\nA densidade da água na temperatura {} K é {} kg/m³\n'.format(temperatura, round(densidade(temperatura), 1)))
+                    print('\nA densidade da água na temperatura {} K é {} kg/m³\n'.format(temperatura, round(calculator.densidade(), 1)))
                     break
                 elif escolha == 2:
-                    print('\nO calor específico da água na temperatura {} K é {} kJ/kg K\n'.format(temperatura, round(calor_especifico(temperatura), 3)))
+                    print('\nO calor específico da água na temperatura {} K é {} kJ/kg K\n'.format(temperatura, round(calculator.calor_especifico(), 3)))
                     break
                 elif escolha == 3:
-                    print('\nA viscosidade da água na temperatura {} K é {} uPa s\n'.format(temperatura, round(viscosidade(temperatura), 1)))
+                    print('\nA viscosidade da água na temperatura {} K é {} uPa s\n'.format(temperatura, round(calculator.viscosidade(), 1)))
                     break
                 elif escolha == 4:
-                    print('/nA condutividade térmica da água na temperatura {} K é {} J/s m K\n'.format(temperatura, round(condutividade_termica(temperatura), 4)))
+                    print('/nA condutividade térmica da água na temperatura {} K é {} J/s m K\n'.format(temperatura, round(calculator.ondutividade_termica(), 4)))
                     break
                 elif escolha == 5:
-                    print('\nA pressão de vapor da água na temperatura {} K é {} kPa\n'.format(temperatura, round(pressao_vapor(temperatura), 1)))
+                    print('\nA pressão de vapor da água na temperatura {} K é {} kPa\n'.format(temperatura, round(calculator.pressao_vapor(), 1)))
                     break
                 elif escolha == 6:
-                    valores = [densidade(temperatura), calor_especifico(temperatura), viscosidade(temperatura), condutividade_termica(temperatura), pressao_vapor(temperatura)]
+                    valores = [calculator.densidade(), calculator.calor_especifico(), calculator.viscosidade(), calculator.condutividade_termica(), calculator.pressao_vapor()]
                     unidade = ['kg/m³', 'kJ/kg K', 'uPa s', 'J/s m K', 'kPa']
                     ind = ['Densidade', 'Calor Específico', 'Condutividade Térmica', 'Condutividade Térmica', 'Pressão de Vapor']
                     df = pd.DataFrame(data = list(zip(valores, unidade)), columns = ['Valor', 'Unidade'], index = ind)
